2901766a3d1ebe14ebf058d637179b3d53005ace
Home <Home@DESKTOP-BP0DVQD>
2023-06-29 02:08:37 +0300

    Add project files.

---
+++ _14.py
@@ -0,0 +1,12 @@
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+def inp(x):
+    if not x:
+        print("End of List")
+    else:
+        print(x[0])
+        x.pop(0)
+        inp(x)
+    
+
+inp(my_list)
